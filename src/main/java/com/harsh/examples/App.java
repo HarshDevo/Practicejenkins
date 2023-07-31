925cc51767ccfbcdc4851fbfec4788baf6540c00
HarshDevo <harshpundir1999@gmail.com>
2023-07-31 14:00:15 +0000

    first commit

---
+++ src/main/java/com/harsh/examples/App.java
@@ -0,0 +1,15 @@
+package com.harsh.examples;
+import my_mathe.Mymath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Mymath obj = new Mymath();
+        System.out.println(obj.SumTwo(10,20));
+    }
+}
